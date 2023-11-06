5464e1ba4df3e7b4331734075452ac7574bb5219
Manuel9593 <mparraodriguez95@gmail.com>
2023-11-06 14:41:52 +0100

    tag di spiegazione a es git 3

--- src/pages/pageParraRodriguez.tsx
+++ src/pages/pageParraRodriguez.tsx
@@ -1,7 +1,12 @@
 import Logo from "../components/parrarodriguez/Logo";
 
 const PageParraRodriguez = () => {
-    return <Logo />;
+    return (<>
+        <Logo />
+        <p>Si esegue un <code>git commit &lt;nuova branch&gt;</code> per creare una nupva branch dall'ultimo commit</p>
+        <p>E <code>git reset --hard HEAD~n</code> o <code>git reset --hard  &lt;codice commit&gt;</code> per quanti commit volete spostare la branch d'inizio</p>
+    </>
+    );
 }
 
-export default PageParraRodriguez;
+export default PageParraRodriguez;~
