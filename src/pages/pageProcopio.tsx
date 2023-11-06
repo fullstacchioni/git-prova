44ced3f50c58d391d49540cca8ddb14d79905cd6
Gabriele <gprocopio50@gmail.com>
2023-11-06 14:41:47 +0100

    risposto alla domanda come fare ad aggioranare le modifiche fatte sulla master sulla mia branch

--- src/pages/pageProcopio.tsx
+++ src/pages/pageProcopio.tsx
@@ -9,6 +9,12 @@ const PageProcopio = () => {
         ho fatto un git checkout su procpio-init e poi ho fatto un git rebase
         master
       </p>
+      <p>
+        la mia branch dedicata è indietro di qualche commit rispetto a master,
+        come faccio a portarla avanti? Cioè, come faccio a portare le modifiche
+        fatte su master sulla mia branch dedicata?
+      </p>
+      <p>devo fare un git merge</p>
     </>
   );
 };
